9ef8d9c90f17b17ae92e9965d4144ed1ef74bba5
Luiz Tanure <letanure@gmail.com>
2017-12-22 13:42:22 +0100

    run styles only

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    'style': './src/style.css',
+  },
   output: {
-    filename: 'bundle.js'
-  }
+    path: path.join(__dirname, './build/'),
+    filename: '[name].css'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ],
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
 };
